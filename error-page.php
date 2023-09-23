4fb275bcaefcfc6e99386fb42cb5374c62b0c5b7
LAPTOP-7IMPE5DI\loydc <loydcose.01@gmail.com>
2023-09-23 13:16:20 +0800

    enhance error page

--- error-page.php
+++ error-page.php
@@ -5,17 +5,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="pico-master/css/pico.min.css">
-    <link rel="stylesheet" href="./css/error-page.css">
+    <link rel="stylesheet" href="./css/output.css">
     <title>Error</title>
 </head>
 
-<body>
-    <main>
-        <article>
-            <h1>ERROR | 404</h1>
-            <a href="index.php" role="button">Back to Login</a>
-        </article>
+<body class="min-h-screen relative">
+    <img src="./images/dancing-pepe.gif" alt="" class="absolute w-[300px] right-0">
+    <img src="./images/clapping-pepe.gif" alt="" class="absolute w-[300px] left-0">
+    <img src="./images/punching-pepe.gif" alt="" class="absolute w-[300px] bottom-0 left-0">
 
+    <main class="max-w-3xl mx-auto w-11/12 min-h-screen flex">
+        <article class="m-auto">
+            <div class=" w-[300px] h-[300px] mx-auto mb-8 relative">
+                <img src="./images/hat.png" alt="a hat" w="319" height="318"
+                    class="-left-6 bottom-[120px] absolute animate-paper z-10">
+                <img src="./images/mustache.png" alt="a mustache" w="146" height="62"
+                    class="absolute bottom-[85px] left-[50px] animate-paper z-10 w-[146px] h-[62px]">
+
+                <img src="./images/404.png" alt="A person that is very happpyy"
+                    class="w-full h-full object-contain animate-paper">
+            </div>
+            <div class="glitch-wrapper">
+                <div class="glitch" data-glitch="ERROR | 404">ERROR | 404</div>
+            </div>
+            <p class="text-2xl text-center my-6">Walang meron d2...</p>
+            <a href="index.php" role="button"
+                class="text-center bg-violet text-white rounded-lg px-4 py-2 mx-auto block">Back to
+                Login</a>
+        </article>
     </main>
 
 </body>
