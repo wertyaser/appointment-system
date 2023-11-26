8042f74de2a64acba0bc170df9f04c975a5eb9fb
LAPTOP-7IMPE5DI\loydc <loydcose.01@gmail.com>
2023-11-26 23:51:45 +0800

    Add appointment buttons on the navbar

--- view-appointment.php
+++ view-appointment.php
@@ -22,8 +22,6 @@ include("db_connect.php");
   <section class="w-11/12 relative mx-auto">
     <nav class="text-pink border border-pink max-w-xl rounded-full p-4 w-11/12 fixed top-4 left-1/2 -translate-x-1/2 flex items-center gap-3">
       <a href="home.php" class="block mr-auto"><img src="./images/lobot.png" alt="lobot logo" class="w-[30px]  "></a>
-      <button class=""><a href="view-appointment.php">View Appointments</a></button>
-      <button class=""><a href="make-appointment.php">Make Appointment</a></button>
       <button class=""><a href="my-account.php">My Account</a></button>
       <a href="logout.php"><svg class="text-pink" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out">
           <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
