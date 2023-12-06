2d2421a282f18a3d3aa5c7b211106aeeff050bdb
Cedrick Steven <cedricksteven.b@gmail.com>
2023-12-06 22:13:32 +0800

    UI fixes.

--- add-user.php
+++ add-user.php
@@ -50,7 +50,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 <body>
     <div class="backgroundImage min-h-screen bg-cover bg-center isolation relative flex justify-center items-center" style="background-image: url('images/auth-bg.jpg');">
-            <div class="parentDiv mx-auto bg-bckgrd bg-opacity-75 rounded-xl shadow-lg p-4 grid grid-rows-9 grid-flow-col gap-4 w-9/12">
+            <div class="parentDiv mx-auto bg-bckgrd bg-opacity-75 rounded-xl shadow-lg p-12 grid grid-rows-9 grid-flow-col gap-4 w-9/12">
                 <div class="logoDiv row-start-2 col-span-1">
                     <img src="./images/Logo.png" class="mx-auto w-[120px]">
                     <h1 class="font-bold text-2xl text-center text-black mt-4">School Management System</h1>
@@ -58,14 +58,18 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
                 </div>
                 <div class="fieldDiv row-span-4">
                     <form method="post" class="m-auto">
-                    <h1 class="mb-6 text-black text-2xl text-center">Sign Up</h1>
-
+                    <h1 class="mb-4 text- font-bold text-3xl text-left">Sign Up</h1>
+                    <p class="mb-6 text-black text-xs text-left">Create your account to access essential academic details and stay informed.</p>
                     <input type="text" name="fname" placeholder="First name" required autocapitalize="on" class="mb-2 block bg-blue border border-white px-4 py-2 rounded-lg text-black w-full">
                     <input type="text" name="lname" placeholder="Last name" required autocapitalize="on" class="mb-2 block bg-blue border border-white px-4 py-2 rounded-lg text-black w-full">
                     <input type="text" name="email" placeholder="Email" required class="mb-2 block bg-blue border border-white px-4 py-2 rounded-lg text-black w-full">
                     <input type="text" name="course" placeholder="Course" required autocapitalize="on" class="mb-2 block bg-blue border border-white px-4 py-2 rounded-lg text-black w-full">
                     <input type="password" name="password" placeholder="Password" required class="mb-2 block bg-blue border border-white px-4 py-2 rounded-lg text-black w-full">
-                    <input type="date" id="date" name="bday" class="mb-2 block bg-blue border border-white px-4 py-2 rounded-lg text-black w-full">
+                    <div class="w-full">
+                        <span>Birthday</span>
+                        <input type="date" id="birthday" name="birthday" value="<?php echo $birthday ?>" class="border border-black rounded-md text-black mb-3 px-4 py-2 w-full justify-center bg-white bg-opacity-50"/>
+                    </div>
+                    <!-- <input type="date" id="date" name="bday" class="mb-2 block bg-blue border border-white px-4 py-2 rounded-lg text-black w-full"> -->
 
                     <div class="flex items-center gap-2 mb-2 flex-wrap">
                         <button type="button" onClick="handleClearFields()" class="py-2 rounded-md border border-white hover:bg-white/[.5] transition-all text-black w-full">Clear</button>
